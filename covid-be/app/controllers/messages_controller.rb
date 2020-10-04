1cc56b0058fd55d0be31290ac31a851cfc9df4ca
dnamgyal <don.namgyal@hotmail.com>
2020-10-04 00:50:19 -0400

    keys fixed

--- covid-be/app/controllers/messages_controller.rb
+++ covid-be/app/controllers/messages_controller.rb
@@ -1,4 +1,4 @@
-
+require 'dotenv/rails-now'
 require 'twilio-ruby'
 
 class MessagesController < ApplicationController
@@ -11,11 +11,11 @@ class MessagesController < ApplicationController
 
 
     def sendMessage(number)
-        account_sid = 'AC83741c4d4a6c3188e021c0f6a4237708'
-        auth_token = 'c11cf1e6dfb88abdd2ea2665fa159ee9'
+        account_sid = ENV['TWILIO_ACCOUNT_SID']
+        auth_token = ENV['TWILIO_AUTH_TOKEN']
         client = Twilio::REST::Client.new(account_sid, auth_token)
 
-        from = '+12184380831' # Your Twilio number
+        from = ENV['TWILIO_NUMBER'] # Your Twilio number
         to = '+1' + number # Your mobile phone number
 
         client.messages.create(
